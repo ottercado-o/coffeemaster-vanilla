7680870162cda0f59bfe0c248de3fdb6b496a113
otterlovermini <otterlovermini@gmail.com>
2024-12-03 21:36:46 +0800

    Adds app.js and download slides.

---
+++ app.js
@@ -0,0 +1,7 @@
+// These definitions are to reduce verbosity.
+const $ = () => document.querySelector.call(this, arguments);
+const $$ = () => document.querySelectorAll.call(this, arguments);
+HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
+HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
+HTMLElement.prototype.$ = (s) => this.querySelector(s);
+HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
