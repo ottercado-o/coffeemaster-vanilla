fd45bc564a73af588b21fc7aecab92a8dfd3aee7
otterlovermini <otterlovermini@gmail.com>
2024-12-03 21:39:47 +0800

    Adds event binding for initialization.

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
 HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
 HTMLElement.prototype.$ = (s) => this.querySelector(s);
 HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
+
+
+// Event binding for initialization
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM is ready");
+});
